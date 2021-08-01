85a94791eed0dcf492dd6bd09b0696ae5f7097e7
songujin <songuzin@naver.com>
2021-08-01 13:38:31 +0900

    조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,9 +1,8 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.annotation.MainDiscountPolicy;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component

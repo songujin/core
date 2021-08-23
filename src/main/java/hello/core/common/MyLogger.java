70ce7b56f973316cd1fad0b87289df867b007ace
songujin <songuzin@naver.com>
2021-08-23 21:53:53 +0900

    스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,8 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
-public class MyLogger {
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class  MyLogger {
 
     private String uuid;
     private String requestURL;
